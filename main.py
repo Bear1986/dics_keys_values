590c8bb0f4c40b1e5d2eea7195f5bcffcd9f67bb
Art Beckett <89126163+Bear1986@users.noreply.github.com>
2021-10-02 07:57:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+student_grades= [9.1, 8.8, 10.0]
+student_grades= {"Harry":8.8,"Joe":9.1,"Sam":10.0}
+
+values = (student_grades.values())
+keys = (student_grades.keys())
+
+print(values)
+print(keys)
